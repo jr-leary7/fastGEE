1023d604c1b7a8191ab515a74980b4a05d043401
Jack Leary <j.leary@ufl.edu>
2024-10-08 20:56:40 -0400

    first bits of R & C++ code

---
+++ R/fastGEE-package.R
@@ -0,0 +1,6 @@
+## usethis namespace: start
+#' @import RcppEigen
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib fastGEE, .registration = TRUE
+## usethis namespace: end
+NULL
---
+++ R/fastGEE.R
@@ -0,0 +1,10 @@
+#' Fit GEEs quickly. 
+#' 
+#' @name fastGEE
+#' @author Jack R. Leary
+#' @description This is the main fitting function of the package. 
+#' @export
+
+fastGEE <- function(model.formula = NULL) {
+  
+}
