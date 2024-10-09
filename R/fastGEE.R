366b2d38f04b5f7f250202705defbebd4bd84568
Jack Leary <j.leary@ufl.edu>
2024-10-08 22:24:06 -0400

    some basic error checking

--- R/fastGEE.R
+++ R/fastGEE.R
@@ -8,10 +8,12 @@
 #' @param id A character specifying the name of the ID variable. Defaults to NULL. 
 #' @param family The family of the model to be fitted. Defaults to \code{\link[stats]{gaussian}} with the identity link. 
 #' @param cor.structure A character specifying the desired correlation structure. Defaults to "independence". 
-#' @param n.cores (Optional) An integer specifying the number of cores to be used in Eigen matrix operations. Defaults to 1. 
+#' @param n.cores (Optional) An integer specifying the number of cores to be used in \code{Eigen} matrix operations. Defaults to 1. 
 #' @param epsilon (Optional) A double specifying the desired tolerance. Defaults to 1e-5. 
 #' @param max.iter (Optional) An integer specifying the maximum number of iterations. Defaults to 20. 
-#' @importFrom stats gaussian 
+#' @importFrom stats gaussian model.frame model.response model.matrix 
+#' @importFrom rlang sym
+#' @importFrom dplyr mutate 
 #' @export
 
 fastGEE <- function(formula = NULL, 
@@ -25,5 +27,27 @@ fastGEE <- function(formula = NULL,
   # check inputs
   if (is.null(formula) || is.null(data) || is.null(id)) { stop("Please provide all necessary arguments to fastGEE().") }
   if (!inherits(formula, "formula")) { stop("Please provide a properly-formatted formula.") }
+  if (!inherits(data, "data.frame")) {
+    data <- try({ as.data.frame(data) }, silent = TRUE)
+  }
+  if (inherits(data, "try-error")) { stop("Argument data must be coercable to an object of class data.frame.") }
+  if (!inherits(id, "character")) { stop("Argument id must be of class character.") }
+  if (!id %in% colnames(data)) { stop("Argument id must be one of the columns of data.") }
+  if (is.unsorted(data[[id]])) { stop("Argument data must be sorted by id.") }
+  if (!inherits(family, "family")) { stop("Argument family must be of class family.") }
+  if (!inherits(cor.structure, "character")) { stop("Argument cor.structure must be of class character.") }
+  cor.structure <- tolower(cor.structure)
+  if (!cor.structure %in% c("independence", "exchangeable", "ar1", "unstructured")) { stop("Please provide a valid correlation structure.") }
+  # create model matrix from formula 
+  model_frame <- stats::model.frame(formula, data)
+  y <- stats::model.response(model_frame)
+  X <- stats::model.matrix(attr(model_frame, "terms"), data = model_frame)
+  # make sure ID is formatted correctly 
+  id_sym <- rlang::sym(id)
+  if (is.factor(data[[id]])) {
+    data <- dplyr::mutate(data, !!id_sym := as.integer(!!id_sym))
+  } else if (is.character(data[[id]])) {
+    data <- dplyr::mutate(data, !!id_sym := as.integer(as.factor(!!id_sym)))
+  }
   
 }
